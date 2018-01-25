0198fe9aeaaf1e9fc21bee8a65cc256b9b6b98c4
funemployeddad <funemployeddad@gmail.com>
2018-01-24 17:33:26 -0700

    got updated options created, just need to add the modern format info and build modern scraper

--- lib/metagame/cli.rb
+++ lib/metagame/cli.rb
@@ -1,33 +1,58 @@
 class Metagame::CLI
-attr_reader :name, :price, :meta_percent, :url
+attr_reader :name, :price, :meta_percent, :url, :format
+
+  def initialize
+    @format = nil
+  end
 
   def call
-    list_format_meta
+    pick_format
     menu
     goodbye
   end
 
-  def list_format_meta
-    puts "Standard Metagame:"
+  def pick_format
+    puts "Which format would you like to view? Standard or Modern?"
+    input = nil
+    input = gets.strip.downcase
+    if input == "standard"
+      @format = "standard"
+      list_standard_meta
+    elsif input == "modern"
+      @format = "modern"
+      list_modern_meta
+    else
+      puts "Gotta pick one or the other... You can do it!"
+      pick_format
+    end
+  end
+
+    def list_standard_meta
+    puts "\nStandard Metagame:\n------------------\n"
     @decks = Metagame::Deck.scrape_standard
     @decks.each.with_index(1) do |deck, i|
-      puts "#{i}. #{deck[:name]} - #{deck[:price]} - #{deck[:meta_percent]} of Meta - https://www.mtggoldfish.com#{deck[:url]}"
+      puts "\n#{i}. #{deck[:name]} - #{deck[:price]} - #{deck[:meta_percent]} of Meta - https://www.mtggoldfish.com#{deck[:url]}"
     end
+  end
+
+  def list_modern_meta
   end
 
   def menu
     input = nil
     while input != "exit"
-      puts "Enter the number for the deck you would like to view, type list to see the decks again, or type exit."
+      puts "\nOPTIONS\n--------\nEnter the number for the deck you would like to view\nType list to see the decks again\nType format to pick your format\nType exit."
       input = gets.strip.downcase
 
       if input.to_i > 0 && input.to_i <= @decks.length
         the_deck = @decks[input.to_i-1]
-        puts "#{the_deck[:name]} - #{the_deck[:price]} - #{the_deck[:meta_percent]} of Meta - https://www.mtggoldfish.com#{the_deck[:url]}"
+        puts "\n#{the_deck[:name]} - #{the_deck[:price]} - #{the_deck[:meta_percent]} of Meta - https://www.mtggoldfish.com#{the_deck[:url]}"
       elsif input == "list"
-        list_format_meta
+        @format == "standard" ? list_standard_meta : list_modern_meta
+      elsif input == "format"
+        pick_format
       else
-        puts "Please try again by typing list or exit."
+        puts "Please try again by typing list, format, or exit."
       end
     end
   end
