6cdc12c1a19a256be2fa28164d4c32d296d78831
github username <github email address>
2018-01-25 14:43:13 -0700

    Got @format working throughout, updated menu options

--- lib/metagame/deck.rb
+++ lib/metagame/deck.rb
@@ -2,7 +2,7 @@ class Metagame::Deck
   attr_accessor :name, :price, :meta_percent, :url
 
   def self.scrape_format(format)
-    doc = Nokogiri::HTML(open("https://www.mtggoldfish.com/metagame/#{@format}#paper"))
+    doc = Nokogiri::HTML(open("https://www.mtggoldfish.com/metagame/#{format}#paper"))
     decks = []
     deck_list ||= doc.search("div.archetype-tile")
     deck_list.collect do |deck|
@@ -14,38 +14,5 @@ class Metagame::Deck
       }
     end
   end
-
-
-
-
-
-    # def self.scrape_standard
-    #   doc = Nokogiri::HTML(open("https://www.mtggoldfish.com/metagame/standard#paper"))
-    #
-    #   decks = []
-    #   deck_list ||= doc.search("div.archetype-tile")
-    #   deck_list.collect do |deck|
-    #     hash = {
-    #       :name => deck.search("h2 span.deck-price-paper a").text,
-    #       :price => deck.search("td.text-right span.deck-price-paper").text.gsub("\n", ""),
-    #       :meta_percent => deck.search("td.percentage.col-freq").text.gsub("\n", ""),
-    #       :url => deck.search("h2 span.deck-price-paper a").attribute("href").value,
-    #     }
-    #   end
-    # end
-
-    #   def self.scrape_modern
-    #     doc = Nokogiri::HTML(open("https://www.mtggoldfish.com/metagame/modern#paper"))
-    #     decks = []
-    #     deck_list ||= doc.search("div.archetype-tile")
-    #     deck_list.collect do |deck|
-    #       hash = {
-    #         :name => deck.search("h2 span.deck-price-paper a").text,
-    #         :price => deck.search("td.text-right span.deck-price-paper").text.gsub("\n", ""),
-    #         :meta_percent => deck.search("td.percentage.col-freq").text.gsub("\n", ""),
-    #         :url => deck.search("h2 span.deck-price-paper a").attribute("href").value,
-    #       }
-    #     end
-    #   end
 
 end
